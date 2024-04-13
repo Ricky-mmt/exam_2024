1689842808545923694e51aba9284c0762809c87
Ricky-mmt <145645266+Ricky-mmt@users.noreply.github.com>
2024-04-13 13:16:49 -0700

    Add files via upload

---
+++ exam/src/components/Products/Product/Product.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Button,
+  CardActionArea,
+} from "@material-ui/core";
+import { AddShoppingCart } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+import useStyles from "./styles";
+
+const Product = ({ product, onAddToCart }) => {
+  const classes = useStyles();
+
+  // Generate a random price between 20,000 and 40,000 shillings
+  const randomPrice = Math.floor(Math.random() * 20000) + 20000;
+
+  return (
+    <Card className={classes.root}>
+      <Link to={`product-view/${product.id}`}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image={product.media.source}
+            title={product.name}
+          />
+        </CardActionArea>
+      </Link>
+      <CardContent>
+        <div className={classes.cardContent}>
+          <p className={classes.cardContentName}> {product.name}</p>
+        </div>
+        <div className={classes.cardContent}>
+          <p className={classes.cardContentPrice}>
+            <b>{randomPrice} Shs</b>
+          </p>
+        </div>
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <Button
+          variant="contained"
+          className={classes.button}
+          endIcon={<AddShoppingCart />}
+          onClick={() => onAddToCart(product.id, 1)}
+        >
+          <b>ADD TO CART</b>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
